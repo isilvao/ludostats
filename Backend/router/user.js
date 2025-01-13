2c13e137343c19a329ca9f7162007610f43d4b8b
Ivan Silva <isilvao@unal.edu.co>
2025-01-12 22:50:54 -0500

    Creacion, lectura, actualizacion y eliminacion de usuarios
    Se crearon todos los endpoints para los usuarios, hace falta refinar el tema del registro de los usuarios por links de invitación a determinados clubes

--- Backend/router/user.js
+++ Backend/router/user.js
@@ -1,10 +1,16 @@
 const express = require('express')
+const multiparty = require('connect-multiparty')
 const userController = require('../controllers/user')
 const md_auth = require('../middleware/authenticate')
 
 const api = express.Router()
+const md_upload = multiparty({uploadDir: './uploads/usersFoto'})
+
 
 api.get('/user/me', [md_auth.asureAuth],userController.getMe)
 api.get('/users', [md_auth.asureAuth], userController.getUsers)
+api.post('/user', [md_auth.asureAuth, md_upload], userController.createUser)
+api.patch('/user/:id', [md_auth.asureAuth, md_upload], userController.updateUser)
+api.delete('/user/:id', [md_auth.asureAuth], userController.deleteUser)
 
 module.exports = api
