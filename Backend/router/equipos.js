a334936a8cff37751ac2b397be9f8a88ecac3a8f
Ivan Silva <isilvao@unal.edu.co>
2025-01-30 22:51:53 -0500

    Agregar middleware para las consultas en los equipos

--- Backend/router/equipos.js
+++ Backend/router/equipos.js
@@ -4,23 +4,24 @@ const multipart = require("connect-multiparty");
 
 const md_auth = require("../middleware/authenticate");
 const md_upload = multipart({ uploadDir: "./uploads/equipos" });
+const md_user = require("../middleware/userValidation");
 
 const api = express.Router();
 
 // Crear un equipo
-api.post("/nuevoequipo", [md_auth.asureAuth, md_upload], equipoController.crearEquipo);
+api.post("/nuevoequipo", [md_auth.asureAuth, md_upload, md_user.validateAdmin], equipoController.crearEquipo);
 
 // Modificar un equipo por su ID
-api.patch("/patchequipo/:id", [md_auth.asureAuth], equipoController.modificarEquipo);
+api.patch("/patchequipo/:id", [md_auth.asureAuth, md_upload, md_user.validateAdmin], equipoController.modificarEquipo);
 
 // Eliminar un equipo por su ID
-api.delete("/eliminarequipo/:id", [md_auth.asureAuth], equipoController.borrarEquipo);
+api.delete("/eliminarequipo/:id", [md_auth.asureAuth, md_user.validateAdmin], equipoController.borrarEquipo);
 
 // Obtener información completa de un equipo por su ID
 api.get("/equipo/:id", [md_auth.asureAuth], equipoController.obtenerEquipoPorId);
 
 // Obtener los equipos de un gerente
-api.get('/misequipos', [md_auth.asureAuth], equipoController.obtenerMisEquipos);
+api.get('/misequipos', [md_auth.asureAuth, md_user.validateAdmin], equipoController.obtenerMisEquipos);
 
 module.exports = api;
 
