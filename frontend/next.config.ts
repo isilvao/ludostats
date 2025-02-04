9f5314a0e2554dd7b6b1c3e898381a25a9210811
_Diego_Rodriguez_ <dirodriguezd@unal.edu.co>
2025-02-03 23:05:01 -0500

    Cambio para fotos de usuarios google

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,13 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+};
+
+module.exports = {
+  images: {
+    domains: ['lh3.googleusercontent.com'],
+  },
 };
 
 export default nextConfig;
