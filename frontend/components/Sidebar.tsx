6e1b7086825584a459f6f843b44e83f592047172
_Diego_Rodriguez_ <dirodriguezd@unal.edu.co>
2025-02-08 21:41:31 -0500

    Cambios sin importancia

--- frontend/components/Sidebar.tsx
+++ frontend/components/Sidebar.tsx
@@ -2,12 +2,41 @@
 
 import Link from 'next/link';
 import Image from 'next/image';
-import { navItems } from '@/constants';
+// import { navItems } from '@/constants';
 import { usePathname, useParams } from 'next/navigation';
 import { cn } from '@/lib/utils';
 import { useEquipoClub } from '@/hooks/useEquipoClub'; // Importar el hook del contexto
 import { getClubLogo } from '@/lib/utils';
 import LoadingScreen from '@/components/LoadingScreen';
+import { FaCalendarAlt, FaUsers, FaChartBar } from 'react-icons/fa';
+
+const navItems = [
+  {
+    name: 'Calendario',
+    icon: FaCalendarAlt,
+    url: '/[dashboard]/calendar',
+  },
+  {
+    name: 'Miembros',
+    icon: FaUsers,
+    url: '/[dashboard]/members',
+  },
+  {
+    name: 'Estadísticas',
+    icon: FaChartBar,
+    url: '/[dashboard]/statistics',
+  },
+  // {
+  //   name: 'Suscripción',
+  //   icon: <FaVideo />,
+  //   url: '/[dashboard]/subscription',
+  // },
+  // {
+  //   name: 'Others',
+  //   icon: <FaEllipsisH />,
+  //   url: '#',
+  // },
+];
 
 const Sidebar = () => {
   const pathname = usePathname();
@@ -35,19 +64,22 @@ const Sidebar = () => {
     <aside className="sidebar bg-white">
       <nav className="sidebar-nav">
         <ul className="flex flex-1 flex-col gap-6">
-          <Link href={`/${nameTeam}/inicio`} className="lg:w-full">
+          <Link href={`/${nameTeam}`} className="lg:w-full">
             <li
               className={cn(
                 'sidebar-nav-item',
-                pathname === `/${nameTeam}/inicio` && 'shad-active'
+                pathname === `/${nameTeam}` && 'shad-active'
               )}
             >
               <Image
                 src={logo}
                 alt={name}
-                width={40}
-                height={40}
-                className={cn(pathname === `/${nameTeam}/inicio`)}
+                width={48} // Aumentar el tamaño del logo
+                height={48} // Aumentar el tamaño del logo
+                className={cn(
+                  'w-12 h-12 rounded-full', // Ajustar el tamaño del logo
+                  pathname === `/${nameTeam}`
+                )}
               />
               <p className="hidden lg:block">{name}</p>
             </li>
@@ -65,17 +97,15 @@ const Sidebar = () => {
                     'shad-active'
                 )}
               >
-                <Image
-                  src={icon}
-                  alt={name}
-                  width={24}
-                  height={24}
+                <span
                   className={cn(
                     'nav-icon',
                     pathname === url.replace('[dashboard]', nameTeam ?? '') &&
                       'nav-icon-active'
                   )}
-                />
+                >
+                  {icon({ size: 24 })}
+                </span>
                 <p className="hidden lg:block">{name}</p>
               </li>
             </Link>
