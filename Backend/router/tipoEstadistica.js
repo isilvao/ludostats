b2648bccc2806ed124f97458bed092a03b2a775c
Ivan Silva <isilvao@unal.edu.co>
2025-02-22 12:52:11 -0500

    Cambio en los middlewares

--- Backend/router/tipoEstadistica.js
+++ Backend/router/tipoEstadistica.js
@@ -7,9 +7,15 @@ const md_stats = require('../middleware/statsValidation')
 
 const api = express.Router()
 
+/** 
 api.get('/tipoestadistica/club/:id_club', [md_auth.asureAuth, md_clubs.validateAdminOrGerenteOrCoachInClub], tipoEstadisticasController.getTipoEstadisticas) // id del club
 api.post('/:id_club/newtipoestadistica', [md_auth.asureAuth,md_clubs.validateCoachOrGerenteInClub], tipoEstadisticasController.createTipoEstadistica) // id del club
 api.patch('/:id_club/updatetipoestadistica/:id_tipoestadistica', [md_auth.asureAuth,md_clubs.validateCoachOrGerenteInClub, md_stats.validateStatTypeOnClub], tipoEstadisticasController.updateTipoEstadistica) // id del club y del tipo de estadistica
+ * 
+*/
+api.get('/tipoestadistica/club/:id_club', tipoEstadisticasController.getTipoEstadisticas) // id del club
+api.post('/:id_club/newtipoestadistica', tipoEstadisticasController.createTipoEstadistica) // id del club
+api.patch('/:id_club/updatetipoestadistica/:id_tipoestadistica', tipoEstadisticasController.updateTipoEstadistica) // id del club y del tipo de estadistica
 api.delete('/:id_club/deletetipoestadistica/:id_tipoestadistica', tipoEstadisticasController.deleteTipoEstadistica) // id del club y del tipo de estadistica
 api.get('/tipoestadistica/equipo/:id_equipo', [md_auth.asureAuth], tipoEstadisticasController.getTypeStadisticByTeam) // id del tipo de estadistica
 
