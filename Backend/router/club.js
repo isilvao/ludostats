0cec42e2ae67931e2171d928af2d197c8470456c
Ivan Silva <isilvao@unal.edu.co>
2025-02-22 18:51:48 -0500

    Update club.js

--- Backend/router/club.js
+++ Backend/router/club.js
@@ -17,7 +17,7 @@ api.post("/newclub", [md_auth.asureAuth, upload.single("logo")], clubController.
 //api.patch('/updateclub/:id_club', [md_auth.asureAuth, md_upload, md_club.validateGerenteInClub], clubController.updateClub) // id del club
 api.patch('/updateclub/:id_club', clubController.updateClub) // id del club
 
-api.delete('/deleteclub/:id_club', [md_auth.asureAuth, md_club.validateGerenteInClub], clubController.deleteClub) // id del club y del gerente
+api.delete('/deleteclub/:id_club', clubController.deleteClub) // id del club y del gerente
 
 // Ruta para encontrar un club por su ID
 api.get('/club/:id', clubController.encontrarClubPorId);
